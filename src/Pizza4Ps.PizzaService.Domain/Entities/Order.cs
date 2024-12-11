45cb2087c78b1ec637ff41d5e651d78a4bfe00e5
baobibo <ng.hoangbao03@gmail.com>
2024-12-11 16:59:09 +0700

    Update Order entity

--- src/Pizza4Ps.PizzaService.Domain/Entities/Order.cs
+++ src/Pizza4Ps.PizzaService.Domain/Entities/Order.cs
@@ -2,25 +2,29 @@
 
 namespace Pizza4Ps.PizzaService.Domain.Entities
 {
-    public class Order : EntityAuditBase<Guid>
-    {
-        public string? Status { get; set; }
-        public Guid OrderInTableId { get; set; }
+	public class Order : EntityAuditBase<Guid>
+	{
+		public DateTimeOffset StartTime { get; set; }
+		public DateTimeOffset EndTime { get; set; }
+		public string? Status { get; set; }
+		public Guid OrderInTableId { get; set; }
 
-        public virtual OrderInTable OrderInTable { get; set; }
-        public virtual ICollection<OrderItem> OrderItems { get; set; }
-        public virtual ICollection<Feedback> Feedbacks { get; set; }
-        public virtual ICollection<Payment> Payments { get; set; }
-        public virtual ICollection<OrderVoucher> OrderVouchers { get; set; }
+		public virtual OrderInTable OrderInTable { get; set; }
+		public virtual ICollection<OrderItem> OrderItems { get; set; }
+		public virtual ICollection<Feedback> Feedbacks { get; set; }
+		public virtual ICollection<Payment> Payments { get; set; }
+		public virtual ICollection<OrderVoucher> OrderVouchers { get; set; }
 
-        private Order()
-        {
-        }
+		private Order()
+		{
+		}
 
-        public Order(string? status, Guid orderInTableId)
-        {
-            Status = status;
-            OrderInTableId = orderInTableId;
-        }
-    }
+		public Order(DateTimeOffset startTime, DateTimeOffset endTime, string? status, Guid orderInTableId)
+		{
+			StartTime = startTime;
+			EndTime = endTime;
+			Status = status;
+			OrderInTableId = orderInTableId;
+		}
+	}
 }
--- src/Pizza4Ps.PizzaService.Persistence/Configurations/OrderConfigration.cs
+++ src/Pizza4Ps.PizzaService.Persistence/Configurations/OrderConfigration.cs
@@ -5,42 +5,51 @@ using Pizza4Ps.PizzaService.Persistence.Constants;
 
 namespace Pizza4Ps.PizzaService.Persistence.Configurations
 {
-    public class OrderConfigration : IEntityTypeConfiguration<Order>
-    {
-        public void Configure(EntityTypeBuilder<Order> builder)
-        {
-            builder.ToTable(TableNames.Order);
-            builder.HasKey(x => x.Id);
+	public class OrderConfiguration : IEntityTypeConfiguration<Order>
+	{
+		public void Configure(EntityTypeBuilder<Order> builder)
+		{
+			builder.ToTable(TableNames.Order);
+			builder.HasKey(x => x.Id);
 
-            builder.Property(x => x.Status)
-                .HasMaxLength(50)
-                .IsRequired(false);
+			builder.Property(x => x.StartTime)
+				.IsRequired();
 
-            builder.HasOne(x => x.OrderInTable)
-                .WithMany(x => x.Orders)
-                .HasForeignKey(x => x.OrderInTableId)
-                .IsRequired()
-                .OnDelete(DeleteBehavior.Cascade);
+			builder.Property(x => x.EndTime)
+				.IsRequired();
 
-            builder.HasMany(x => x.OrderItems)
-                .WithOne(x => x.Order)
-                .HasForeignKey(x => x.OrderId)
-                .OnDelete(DeleteBehavior.Cascade);
+			builder.Property(x => x.Status)
+				.HasMaxLength(50)
+				.IsRequired(false);
 
-            builder.HasMany(x => x.Feedbacks)
-                .WithOne(x => x.Order)
-                .HasForeignKey(x => x.OrderId)
-                .IsRequired(false);
+			builder.HasOne(x => x.OrderInTable)
+				.WithMany(x => x.Orders)
+				.HasForeignKey(x => x.OrderInTableId)
+				.IsRequired()
+				.OnDelete(DeleteBehavior.Cascade);
 
-            builder.HasMany(x => x.Payments)
-                .WithOne(x => x.Order)
-                .HasForeignKey(x => x.OrderId)
-                .OnDelete(DeleteBehavior.Cascade);
+			builder.HasMany(x => x.OrderItems)
+				.WithOne(x => x.Order)
+				.HasForeignKey(x => x.OrderId)
+				.IsRequired()
+				.OnDelete(DeleteBehavior.Cascade);
 
-            builder.HasMany(x => x.OrderVouchers)
-                .WithOne(x => x.Order)
-                .HasForeignKey(x => x.OrderId)
-                .OnDelete(DeleteBehavior.SetNull);
-        }
-    }
+			builder.HasMany(x => x.Feedbacks)
+				.WithOne(x => x.Order)
+				.HasForeignKey(x => x.OrderId)
+				.IsRequired(false);
+
+			builder.HasMany(x => x.Payments)
+				.WithOne(x => x.Order)
+				.HasForeignKey(x => x.OrderId)
+				.IsRequired()
+				.OnDelete(DeleteBehavior.Cascade);
+
+			builder.HasMany(x => x.OrderVouchers)
+				.WithOne(x => x.Order)
+				.HasForeignKey(x => x.OrderId)
+				.IsRequired(false)
+				.OnDelete(DeleteBehavior.SetNull);
+		}
+	}
 }
