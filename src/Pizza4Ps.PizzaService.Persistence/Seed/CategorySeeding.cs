66e7ffe655552ec12ece8893b4ed38d3ff057028
haovn2411 <haovn2411@gmail.com>
2024-12-28 16:14:40 +0700

    update seeding data base

---
+++ src/Pizza4Ps.PizzaService.Persistence/Seed/CategorySeeding.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Pizza4Ps.PizzaService.Domain.Entities;
+
+namespace Pizza4Ps.PizzaService.Persistence.Seed
+{
+    public class CategorySeeding : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(
+                new Category(Guid.Parse("AD8A1D13-A895-4A94-D20C-08DCF26F96D8"), "Đồ ăn", ""),
+                new Category(Guid.Parse("8BF80BC9-2302-4E36-B4CA-146CE7D34543"), "Đồ uống", "")
+                );
+        }
+    }
+}
--- src/Pizza4Ps.PizzaService.Persistence/Seed/ProductSeeding.cs
+++ src/Pizza4Ps.PizzaService.Persistence/Seed/ProductSeeding.cs
@@ -8,9 +8,24 @@ namespace Pizza4Ps.PizzaService.Persistence.Seed
     {
         public void Configure(EntityTypeBuilder<Product> builder)
         {
-            //builder.HasData(
-
-            //);
+            builder.HasData(
+                new Product
+                {
+                    Id = Guid.Parse("AD8A1D13-A895-4A94-D20C-08DCF26F96D8"),
+                    CategoryId = Guid.Parse("AD8A1D13-A895-4A94-D20C-08DCF26F96D8"),
+                    Name = "Pizza hut",
+                    Price = 1000,
+                    Description = "Pizza"
+                },
+                new Product
+                {
+                    Id = Guid.Parse("8BF80BC9-2302-4E36-B4CA-146CE7D34543"),
+                    CategoryId = Guid.Parse("8BF80BC9-2302-4E36-B4CA-146CE7D34543"),
+                    Name = "Pizza In",
+                    Price = 1200,
+                    Description = "Pizza"
+                }
+            );
         }
     }
 }
