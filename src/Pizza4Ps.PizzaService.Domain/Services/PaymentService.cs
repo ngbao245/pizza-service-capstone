44ef03e10ea6df9126885677720980fe89a10913
haovn2411 <haovn2411@gmail.com>
2025-02-21 02:16:46 +0700

    test payment webhook

--- src/Pizza4Ps.PizzaService.Domain/Services/PaymentService.cs
+++ src/Pizza4Ps.PizzaService.Domain/Services/PaymentService.cs
@@ -46,17 +46,17 @@ namespace Pizza4Ps.PizzaService.Domain.Services
         public async Task<bool> ProcessWebhookData(WebhookType webhookData)
         {
             // Xác thực và lấy thông tin từ webhook thông qua gateway PayOS
-            var result = _payOsService.VerifyPaymentWebhookData(webhookData);
+            var result =  _payOsService.VerifyPaymentWebhookData(webhookData);                
+            // Giả sử result.OrderCode tương ứng với OrderId
+            var payment = await _paymentRepository.GetSingleAsync(x => x.OrderCode == webhookData.code.ToString());
+            if (payment != null)
+            {
+                payment.SetPaid();
+                _paymentRepository.Update(payment);
+            }
             if (result != null && result.code == "00")
             {
-                // Giả sử result.OrderCode tương ứng với OrderId
-                var payment = await _paymentRepository.GetSingleAsync(x => x.OrderCode == result.orderCode.ToString());
-                if (payment != null)
-                {
-                    payment.SetPaid();
-                    _paymentRepository.Update(payment);
-                }
-                var order = await _orderRepository.GetSingleAsync(x => x.OrderCode == result.orderCode.ToString());
+                var order = await _orderRepository.GetSingleAsync(x => x.OrderCode == webhookData.code.ToString());
                 if (order != null)
                 {
                     order.SetPaid();
@@ -65,6 +65,7 @@ namespace Pizza4Ps.PizzaService.Domain.Services
                 await _unitOfWork.SaveChangeAsync();
                 return true;
             }
+            await _unitOfWork.SaveChangeAsync();
             return false;
         }
         public long GenerateOrderCode()
