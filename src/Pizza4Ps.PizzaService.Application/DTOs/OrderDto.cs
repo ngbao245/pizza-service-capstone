7df5c327fb1a87f73a455516b6943180ba368fd6
haovn2411 <haovn2411@gmail.com>
2025-02-24 20:43:33 +0700

    update order dto

--- src/Pizza4Ps.PizzaService.Application/DTOs/OrderDto.cs
+++ src/Pizza4Ps.PizzaService.Application/DTOs/OrderDto.cs
@@ -10,7 +10,7 @@ namespace Pizza4Ps.PizzaService.Application.DTOs
         public string? OrderCode { get; set; }
         public DateTimeOffset StartTime { get; set; }
         public DateTimeOffset? EndTime { get; set; }
-        public OrderStatusEnum Status { get; set; }
+        public string Status { get; set; }
         public Guid TableId { get; set; }
 
         public virtual TableDto Table { get; set; }
--- src/Pizza4Ps.PizzaService.Application/Mappers/OrderProfile.cs
+++ src/Pizza4Ps.PizzaService.Application/Mappers/OrderProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Pizza4Ps.PizzaService.Application.DTOs;
 using Pizza4Ps.PizzaService.Domain.Entities;
+using Pizza4Ps.PizzaService.Domain.Enums;
 
 namespace Pizza4Ps.PizzaService.Application.Mappers
 {
@@ -8,7 +9,8 @@ namespace Pizza4Ps.PizzaService.Application.Mappers
     {
         public OrderProfile()
         {
-            CreateMap<OrderDto, Order>().ReverseMap();
+            CreateMap<OrderDto, Order>().ReverseMap()
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => Enum.GetName(typeof(OrderStatusEnum), src.Status!)));
         }
     }
 }
