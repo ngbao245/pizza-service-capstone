49f479bf9c335d54fee2277158f5f5396804eb7f
haovn2411 <haovn2411@gmail.com>
2025-03-23 01:49:46 +0700

    update customer dto

--- src/Pizza4Ps.PizzaService.Application/DTOs/CustomerDto.cs
+++ src/Pizza4Ps.PizzaService.Application/DTOs/CustomerDto.cs
@@ -1,9 +1,25 @@
-﻿namespace Pizza4Ps.PizzaService.Application.DTOs
+﻿using Pizza4Ps.PizzaService.Domain.Entities.Identity;
+
+namespace Pizza4Ps.PizzaService.Application.DTOs
 {
     public class CustomerDto
     {
         public Guid Id { get; set; }
-        public string FullName { get; set; }
-        public string Phone { get; set; }
+
+        public string? FullName { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? VerifiedCodeEmail { get; set; }
+
+        public bool IsVerifiedEmail { get; set; }
+
+        public bool? Gender { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public string? Email { get; set; }
     }
 }
