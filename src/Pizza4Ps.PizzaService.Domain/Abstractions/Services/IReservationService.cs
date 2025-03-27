8963013099f3c4b7fe675dbce360d0f37cc799a5
haovn2411 <haovn2411@gmail.com>
2025-03-28 01:12:06 +0700

    update reserve table

--- src/Pizza4Ps.PizzaService.Domain/Abstractions/Services/IReservationService.cs
+++ src/Pizza4Ps.PizzaService.Domain/Abstractions/Services/IReservationService.cs
@@ -4,7 +4,7 @@ namespace Pizza4Ps.PizzaService.Domain.Abstractions.Services
 {
     public interface IReservationService : IDomainService
     {
-        Task<Guid> CreateAsync(Guid? customerCode, string customerName, string phoneNumber, DateTime bookingTime, int numberOfPeople);
+        Task<Guid> CreateAsync(string customerName, string phoneNumber, DateTime bookingTime, int numberOfPeople);
         Task<Guid> UpdateAsync(Guid id, DateTime bookingDate, int guestCount, string status, Guid customerId);
         Task DeleteAsync(List<Guid> ids, bool IsHardDeleted = false);
         Task RestoreAsync(List<Guid> ids);
