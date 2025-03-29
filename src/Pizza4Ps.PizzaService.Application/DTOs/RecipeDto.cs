bada45d449dad73c3a21317eb9c375db2b9165b6
BaoBiBo <ng.hoangbao03@gmail.com>
2025-03-29 20:29:45 +0700

    hot fix

--- src/Pizza4Ps.PizzaService.Application/DTOs/RecipeDto.cs
+++ src/Pizza4Ps.PizzaService.Application/DTOs/RecipeDto.cs
@@ -5,6 +5,7 @@ namespace Pizza4Ps.PizzaService.Application.DTOs
 {
     public class RecipeDto
     {
+        public Guid Id { get; set; }
         public Guid ProductSizeId { get; set; }
         public Guid? IngredientId { get; set; }
         public string IngredientName { get; set; }
