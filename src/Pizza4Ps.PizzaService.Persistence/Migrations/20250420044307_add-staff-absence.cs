6503d63cb77a8b4aef449c8fbb908a5383a508aa
BaoBiBo <ng.hoangbao03@gmail.com>
2025-04-20 11:44:09 +0700

    update migration

---
+++ src/Pizza4Ps.PizzaService.Persistence/Migrations/20250420044307_add-staff-absence.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pizza4Ps.PizzaService.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class addstaffabsence : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StaffAbsence",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StaffId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    WorkingSlotId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AbsentDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    CreatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    ModifiedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    DeletedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    DeletedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StaffAbsence", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StaffAbsence_Staff_StaffId",
+                        column: x => x.StaffId,
+                        principalTable: "Staff",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StaffAbsence_WorkingSlot_WorkingSlotId",
+                        column: x => x.WorkingSlotId,
+                        principalTable: "WorkingSlot",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StaffAbsence_StaffId",
+                table: "StaffAbsence",
+                column: "StaffId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StaffAbsence_WorkingSlotId",
+                table: "StaffAbsence",
+                column: "WorkingSlotId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StaffAbsence");
+        }
+    }
+}
