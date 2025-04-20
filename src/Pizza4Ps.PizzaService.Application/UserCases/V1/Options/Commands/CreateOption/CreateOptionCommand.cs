0b731fbd562e1802fa31446843aa8c4263373334
haovn2411 <haovn2411@gmail.com>
2025-04-21 02:25:48 +0700

    update product option

--- src/Pizza4Ps.PizzaService.Application/UserCases/V1/Options/Commands/CreateOption/CreateOptionCommand.cs
+++ src/Pizza4Ps.PizzaService.Application/UserCases/V1/Options/Commands/CreateOption/CreateOptionCommand.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Pizza4Ps.PizzaService.Application.Abstractions;
+using Pizza4Ps.PizzaService.Application.UserCases.V1.OptionItems.Commands.CreateOptionItem;
 
 namespace Pizza4Ps.PizzaService.Application.UserCases.V1.Options.Commands.CreateOption
 {
@@ -8,5 +9,6 @@ namespace Pizza4Ps.PizzaService.Application.UserCases.V1.Options.Commands.Create
         public string Name { get; set; }
         public string? Description { get; set; }
         public bool SelectMany { get; set; }
+        public List<CreateOptionItemCommand> OptionItems { get; set; }
     }
 }
