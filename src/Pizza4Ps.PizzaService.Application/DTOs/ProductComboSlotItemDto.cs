003625f81b2a9817c5e55805b826533057144d56
haovn2411 <haovn2411@gmail.com>
2025-04-23 18:06:15 +0700

    update product combólot item dto

--- src/Pizza4Ps.PizzaService.Application/DTOs/ProductComboSlotItemDto.cs
+++ src/Pizza4Ps.PizzaService.Application/DTOs/ProductComboSlotItemDto.cs
@@ -1,9 +1,12 @@
-﻿namespace Pizza4Ps.PizzaService.Application.DTOs
+﻿using Pizza4Ps.PizzaService.Domain.Entities;
+
+namespace Pizza4Ps.PizzaService.Application.DTOs
 {
     public class ProductComboSlotItemDto
     {
         public Guid Id { get; set; }
         public Guid ProductId { get; set; }
         public virtual ProductDto Product { get; set; } = null!; // Món thành phần trong combo
+        public decimal ExtraPrice { get; set; }
     }
 }
