b1bf4a39e74ae8e25a00e8fb01f76afacef8cecf
haovn2411 <haovn2411@gmail.com>
2025-05-03 11:15:53 +0700

    update produict profile

--- src/Pizza4Ps.PizzaService.Application/Mappers/ProductProfile.cs
+++ src/Pizza4Ps.PizzaService.Application/Mappers/ProductProfile.cs
@@ -10,7 +10,10 @@ namespace Pizza4Ps.PizzaService.Application.Mappers
 		public ProductProfile()
 		{
             CreateMap<ProductDto, Product>().ReverseMap()
-                .ForMember(dest => dest.ProductType, opt => opt.MapFrom(src => Enum.GetName(typeof(ProductTypeEnum), src.ProductType)))
+                .ForMember(dest => dest.ProductType,
+                    opt => opt.MapFrom(src => src.ProductType.HasValue
+                        ? Enum.GetName(typeof(ProductTypeEnum), src.ProductType.Value)
+                        : null))
                 .ForMember(dest => dest.ProductRole, opt => opt.MapFrom(src => Enum.GetName(typeof(ProductRoleEnum), src.ProductRole)))
                 .ForMember(dest => dest.ProductStatus, opt => opt.MapFrom(src => Enum.GetName(typeof(ProductStatus), src.ProductStatus)));
 
