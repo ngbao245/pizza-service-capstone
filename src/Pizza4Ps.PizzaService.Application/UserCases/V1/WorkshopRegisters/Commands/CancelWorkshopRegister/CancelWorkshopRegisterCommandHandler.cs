7f60f23178b6347d2b3c6f014a55c25ab273cce9
haovn2411 <haovn2411@gmail.com>
2025-05-08 01:13:13 +0700

    update cancel workshop register

--- src/Pizza4Ps.PizzaService.Application/UserCases/V1/WorkshopRegisters/Commands/CancelWorkshopRegister/CancelWorkshopRegisterCommandHandler.cs
+++ src/Pizza4Ps.PizzaService.Application/UserCases/V1/WorkshopRegisters/Commands/CancelWorkshopRegister/CancelWorkshopRegisterCommandHandler.cs
@@ -6,11 +6,14 @@ namespace Pizza4Ps.PizzaService.Application.UserCases.V1.WorkshopRegisters.Comma
 {
     public class CancelWorkshopRegisterCommandHandler : IRequestHandler<CancelWorkshopRegisterCommand>
     {
+        private readonly IWorkshopRepository _workshopRepository;
         private readonly IUnitOfWork _unitOfWork;
         private readonly IWorkshopRegisterRepository _workshopRegisterRepository;
         public CancelWorkshopRegisterCommandHandler(IUnitOfWork unitOfWork,
-            IWorkshopRegisterRepository workshopRegisterRepository)
+            IWorkshopRegisterRepository workshopRegisterRepository,
+            IWorkshopRepository workshopRepository)
         {
+            _workshopRepository = workshopRepository;
             _unitOfWork = unitOfWork;
             _workshopRegisterRepository = workshopRegisterRepository;
         }
@@ -22,7 +25,14 @@ namespace Pizza4Ps.PizzaService.Application.UserCases.V1.WorkshopRegisters.Comma
                 throw new Exception("Workshop register not found");
             }
             workshopRegister.Cancel(request.Reason);
+            var workshop = await _workshopRepository.GetSingleAsync(x => x.Id == workshopRegister.WorkshopId);
+            if (workshop == null)
+            {
+                throw new Exception("Workshop not found");
+            }
+            workshop.totalRegisteredParticipant = workshop.totalRegisteredParticipant - 1;
             _workshopRegisterRepository.Update(workshopRegister);
+            _workshopRepository.Update(workshop);
             await _unitOfWork.SaveChangeAsync(cancellationToken);
         }
     }
